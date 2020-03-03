e9ef5aeacd1597680e7dfe8b6449e64d47072517
Armando Neto <abiagion@redhat.com>
2020-03-03 15:15:36 -0300

    wip: Troubleshoot Travis env
    Signed-off-by: Armando Neto <abiagion@redhat.com>

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,5 +1,12 @@
 import hello
+import os
 
 
 def test_says_world():
     assert hello.say_what() == 'world'
+
+
+def test_var_env():
+    travis_env = os.environ.get('TRAVIS')
+    print('TRAVIS VAR:', travis_env)
+    assert travis_env == 'true'
